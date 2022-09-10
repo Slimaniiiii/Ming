09b9a2a9953a4b6be7d71d0a28c729f0b7da30d6
Houssem <slimanihoussem997@gmail.com>
2022-09-10 18:08:28 +0100

    setting out routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import { AuthentificationPage } from './pages/AuthentificationPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<AuthentificationPage/>}> </Route>
+        <Route path="/conversations" element={<div><div>conversations</div><Outlet /></div>}>
+          <Route path=":id" element={<div>conversation id page</div>} />
+        </Route>
+
+
+      </Routes>
+
+    </>
   );
 }
 
---
+++ src/pages/AuthentificationPage.tsx
@@ -0,0 +1,7 @@
+export const AuthentificationPage = () => {
+    return (
+        <div>
+        <h1>Authentification Page</h1>
+        </div>
+    );
+    }
